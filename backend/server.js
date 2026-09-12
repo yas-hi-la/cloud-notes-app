3fa2689dedbdebebb9fae2e0067fcbe1b89d332d
yas-hi-la <yashila.amd@gmail.com>
2026-09-13 00:02:14 +0530

    Serve frontend and point to EC2 backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
